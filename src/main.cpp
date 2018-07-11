f25744cb47c82d27402914e7147926430e9bb68b
Michael <michaelfromano@gmail.com>
2018-07-11 10:10:12 -0400

    changes to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <Arduino.h>
 #include "main.h"
+#include "Timer.h"
+
+CAMERA_PIN = 1;
+
 
 void setup() {
   Serial.begin(230200);
+  pinMode(MOTION_PIN,OUTPUT);
+  pinMode(CAMERA_PIN,OUTPUT);
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
+
+    t.update();
 }
