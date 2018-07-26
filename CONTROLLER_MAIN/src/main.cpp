cc71ee5ae910caa55b4b2a6807c4790df417d602
Michael Romano <michaelfromano@gmail.com>
2018-07-26 08:15:36 -0400

    Changes made to piolibdeps

--- CONTROLLER_MAIN/src/main.cpp
+++ CONTROLLER_MAIN/src/main.cpp
@@ -4,22 +4,27 @@
 
 const int ttlpulseout = 2;
 const int cam = 4;
-
+const int matlab = 3;
 
 
 void setup() {
   fastPinMode(ttlpulseout, OUTPUT);
   fastPinMode(cam, OUTPUT);
+  fastPinMode(matlab, INPUT);
+
 }
 
 void loop() {
-  fastDigitalWrite(ttlpulseout, HIGH);
-  while (true) {
-    fastDigitalWrite(cam, HIGH);
-    _delay_cycles(16000);
-    fastDigitalWrite(cam, LOW);
-    _delay_cycles(784000);
-    //Arduino Uno has 16 Mhz clock cycles, may vary if a different board is used
+  while(fastDigitalRead(matlab) == 1){ //checking for input from matlab
+    fastDigitalWrite(ttlpulseout, HIGH);
+    while (true) {
+      fastDigitalWrite(cam, HIGH);
+      _delay_cycles(16000);
+      fastDigitalWrite(cam, LOW);
+      _delay_cycles(784000);
+      //Arduino Uno has 16 Mhz clock cycles, may vary if a different board is used
+      //So, to delay for 49 milliseconds do 16Mhz*0.049s (may not be 16Mhz depending on the board)
+    }
   }
   fastDigitalWrite(ttlpulseout, LOW);
 }
