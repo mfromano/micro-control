2545a12c6399214501421e13b7424e5a813ca800
Michael <michaelfromano@gmail.com>
2018-11-27 16:12:04 -0500

    done

--- experimental_code/TonePuffTestPin/src/main_audio.cpp
+++ experimental_code/TonePuffTestPin/src/main_audio.cpp
@@ -12,7 +12,7 @@
 
 const uint8_t PUFF_PIN = 3; // pin to use for PUFF
 const uint8_t DAC_PIN = A14;
-const bool EXPERIMENT_ON = 0;
+ uint32_t EXPERIMENT_ON;
 
 elapsedMillis experimentTime;
 
@@ -20,17 +20,18 @@ void setup() {
   fastPinMode(PUFF_PIN, OUTPUT);
   fastPinMode(DAC_PIN, OUTPUT);
   AudioMemory(128);
-  experimentTime = 0;
+  EXPERIMENT_ON = 0;
 }
 
 void loop(){
-  while(experimentTime < 2000) {
+  while(EXPERIMENT_ON < 200) {
     fastDigitalWrite(PUFF_PIN,HIGH);
     analogWrite(DAC_PIN, 2000);
     delay(50)
     fastDigitalWrite(PUFF_PIN,LOW);
     analogWrite(DAC_PIN,0);
     delay(1000);
+    EXPERIMENT_ON++;
   }
 
 }
